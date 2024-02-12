0dd661a8471ae62cacf7d387fcfb791ab4ecab0c
niranjanyadav123 <156639862+niranjanyadav123@users.noreply.github.com>
2024-02-13 02:36:48 +0530

    Update and rename app.java to App.java

--- ASS02/Insert_into_database/App.java
+++ ASS02/Insert_into_database/App.java
@@ -15,7 +15,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-public class app {
+public class App {
 
 	public static void main(String[] args) throws Exception {
 		
