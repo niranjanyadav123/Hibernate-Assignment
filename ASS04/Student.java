230a1394d4944354e088574f024d5ba2a773ae76
niranjanyadav123 <156639862+niranjanyadav123@users.noreply.github.com>
2024-02-16 13:00:41 +0530

    Create Student.java

---
+++ ASS04/Student.java
@@ -0,0 +1,54 @@
+/*
+ * Author - Niranjan Bhausaheb Yadav
+ 
+ * Date - 16/02/2024
+ 
+  * About Code - We create the Student Class which implements the Serializable markeable interface.
+   
+ */
+
+
+
+package com.niranjan.entities;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+     private static final long serialVersionUID = 1L;
+     
+     private int rno;
+     private String name;
+     private double per;
+     
+     
+	public int getRno() {
+		return rno;
+	}
+	
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getPer() {
+		return per;
+	}
+	
+	public void setPer(double per) {
+		this.per = per;
+	}
+
+	@Override
+	public String toString() {
+		return "student [rno=" + rno + ", name=" + name + ", per=" + per + "]";
+	}
+     
+     
+}
