b3ef827978e352bdb1f003a018c6a98c1f2295f4
niranjanyadav123 <156639862+niranjanyadav123@users.noreply.github.com>
2024-03-07 01:42:09 +0530

    Create Student.java

---
+++ ASS-Reflection-AccessPrivateMembers/Entities/Student.java
@@ -0,0 +1,25 @@
+/*
+ * Author - Niranjan Bhausaheb Yadav
+ 
+ * Date - 7/03/2024
+ 
+ * About Code - It is Student Class with three data members,parameterized constructors .
+ 
+ */
+
+package com.niranjan.entities;
+
+public class Student {
+    private Integer rno;
+    private String name;
+    private Double per;
+    
+    
+	public Student(Integer rno, String name, Double per) {
+		super();
+		this.rno = rno;
+		this.name = name;
+		this.per = per;
+	}    
+    
+}
